dbe4040ef7fd9b3cf4c4fabf3a66ac934dd07b50
peng.yu <peng.yu@bjrxtd.com>
2020-07-13 14:59:52 +0800

    split dockerfile.

--- php/php-fpm-yaf-xdebug/docker-build.sh
+++ php/php-fpm-yaf-xdebug/docker-build.sh
@@ -1,3 +1,17 @@
 #!/usr/bin/env bash
 
-docker build -t yibuyiyin/php-fpm-yaf-xdebug:$(date '+%Y%m%d') .
+if [ -z "$1" ]; then
+    echo "usage: ./docker-build.sh [test|product]"
+    exit 1
+fi
+
+env=$1
+if [ ! -d $env ]; then
+    echo "error: $env/ not exists."
+    exit 1
+fi
+
+cp -a -f sources.list $env/ && \
+cd $env && \
+docker build -t yibuyiyin/php-fpm-yaf-xdebug:$env-$(date '+%Y%m%d') . && \
+rm sources.list
